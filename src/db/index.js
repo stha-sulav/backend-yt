3a11753f67fc21e908e80620c7a780a982d7e6a5
sulav <sulavshrestha2323@gmail.com>
2023-12-17 22:38:47 +0545

    DB added

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+
+    if (!connection) {
+      throw new Error("Cannot connect to Database");
+    }
+
+    console.log(`Database connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection failed", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
